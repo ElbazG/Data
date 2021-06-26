2e06ba1d466029de86461ef4d801ee9729372381
Gil <elbazg123@gmail.com>
2021-06-26 22:58:04 +0300

    Up

--- classifier.py
+++ classifier.py
@@ -2,9 +2,30 @@ import csv
 import sys
 
 data = sys.argv[1]
+img_list = []
 
-with open(data, newline='') as file:
-    reader = csv.DictReader (file)
-    for row in reader:
-        print(row['img3'], row['key_resp_4.keys'])
 
+def read_data():
+    with open(data, newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['img3'].startswith('exp_1_50'):
+                img_list.append((row['img3'], row['key_resp_4.keys'], 0))
+            elif row['img3'].startswith('exp_2_50'):
+                img_list.append((row['img3'], row['key_resp_4.keys'], 0))
+            elif row['img3'].startswith('exp_3_50'):
+                img_list.append((row['img3'], row['key_resp_4.keys'], 1))
+    return img_list
+
+
+def put_data(var):
+    file = open('results.csv', 'w+', newline='')
+    with file:
+        write = csv.writer(file)
+        write.writerows(var)
+    return file
+
+
+if __name__ == "__main__":
+    x = read_data()
+    results = put_data(x)
